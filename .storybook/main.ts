e7e4bbea9678d26f0e4ee0df1905d56f851dc9b2
basilgood <elsile69@yahoo.com>
2025-09-22 21:00:35 +0300

    feat: add cosmoz-slider storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,25 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
-  'stories': [
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
-  'addons': [],
-  'framework': {
-    'name': '@storybook/web-components-vite',
-    'options': {},
-  },
-  refs: {
-    'cosmoz-dropdown': {
-      title: 'Cosmoz Dropdown Storybook',
-      url: 'https://neovici.github.io/cosmoz-dropdown',
-    },
-    'cosmoz-omnitable': {
-      title: 'Cosmoz Omnitable Storybook',
-      url: 'https://neovici.github.io/cosmoz-omnitable',
-    },
-  },
+	stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [],
+	framework: {
+		name: '@storybook/web-components-vite',
+		options: {},
+	},
+	refs: {
+		'cosmoz-dropdown': {
+			title: 'Cosmoz Dropdown Storybook',
+			url: 'https://neovici.github.io/cosmoz-dropdown',
+		},
+		'cosmoz-omnitable': {
+			title: 'Cosmoz Omnitable Storybook',
+			url: 'https://neovici.github.io/cosmoz-omnitable',
+		},
+		'cosmoz-slider': {
+			title: 'Cosmoz Slider Storybook',
+			url: 'https://neovici.github.io/cosmoz-slider',
+		},
+	},
 };
 export default config;
