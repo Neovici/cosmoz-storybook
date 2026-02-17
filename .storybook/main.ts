0d88074b22729a70e35cbe0c811f44d052b102ed
Cristian Necula <VirusZ@gmail.com>
2026-02-18 01:08:28 +0200

    feat: add 10 missing UI component storybook refs (#150)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -28,6 +28,46 @@ const config: StorybookConfig = {
 			title: 'Cosmoz Input Storybook',
 			url: 'https://neovici.github.io/cosmoz-input',
 		},
+		'cosmoz-autocomplete': {
+			title: 'Cosmoz Autocomplete Storybook',
+			url: 'https://neovici.github.io/cosmoz-autocomplete',
+		},
+		'cosmoz-button': {
+			title: 'Cosmoz Button Storybook',
+			url: 'https://neovici.github.io/cosmoz-button',
+		},
+		'cosmoz-charts': {
+			title: 'Cosmoz Charts Storybook',
+			url: 'https://neovici.github.io/cosmoz-charts',
+		},
+		'cosmoz-command-menu': {
+			title: 'Cosmoz Command Menu Storybook',
+			url: 'https://neovici.github.io/cosmoz-command-menu',
+		},
+		'cosmoz-filter-bar': {
+			title: 'Cosmoz Filter Bar Storybook',
+			url: 'https://neovici.github.io/cosmoz-filter-bar',
+		},
+		'cosmoz-image-viewer': {
+			title: 'Cosmoz Image Viewer Storybook',
+			url: 'https://neovici.github.io/cosmoz-image-viewer',
+		},
+		'cosmoz-rating': {
+			title: 'Cosmoz Rating Storybook',
+			url: 'https://neovici.github.io/cosmoz-rating',
+		},
+		'cosmoz-tabs': {
+			title: 'Cosmoz Tabs Storybook',
+			url: 'https://neovici.github.io/cosmoz-tabs',
+		},
+		'cosmoz-tooltip': {
+			title: 'Cosmoz Tooltip Storybook',
+			url: 'https://neovici.github.io/cosmoz-tooltip',
+		},
+		'cosmoz-treenode-navigator': {
+			title: 'Cosmoz Treenode Navigator Storybook',
+			url: 'https://neovici.github.io/cosmoz-treenode-navigator',
+		},
 	},
 };
 
